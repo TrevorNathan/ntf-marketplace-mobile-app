d82f4872715df77d3ea9aa33e0d3cff195ccebec
TrevorNathan <trevoratwijukire@gmail.com>
2022-12-09 18:55:47 +0300

    ADD: components boilerplate

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,15 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, FlatList, SafeAreaView } from "react-native";
+import { useState } from "react";
+
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
-	return <Text>Home</Text>;
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<Text>Home</Text>
+		</SafeAreaView>
+	);
 };
 
 export default Home;
